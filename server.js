4b196435cd0ad6f7c3b3129754ed72a6e57bddc0
Ay44n-ej4z <ayaan.ejaz185@gmail.com>
2021-12-31 12:53:16 +0530

    Icommit

--- server.js
+++ server.js
@@ -2,19 +2,58 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const noderfc = require("node-rfc");
-const PORT = 5000
-const data = require("./router/data")
+const PORT = 5000;
+const data = require("./router/data");
 
-app.use(express.json())
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 
-app.use(cors({ origin: '*',
-methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-  optionsSuccessStatus: 200
-}) )
+}))
+
+app.use(express.json());
+
+
+app.get('/', async(req, res) => {
+    try {
+        function pad_with_zeroes(number, length) {
+    
+            var my_string = '' + number;
+            
+            while (my_string.length < length) {
+                my_string = '0' + my_string;
+                
+            }
+            
+        
+            return my_string;
+            
+        }
+        var num ;
+        var l;
+        if(num == 1 || 2 ||3 || 4 || 5 || 6 || 7 || 8 || 9 ){
+                l = 10 
+            }else{
+        
+            }
+        console.log(pad_with_zeroes(10, l));
+        
+        // const data = await CallBAPI("BAPI_VENDOR_GETDETAIL", {
+        //     VENDORNO: "0000000015",
+        // });
+        const data = await CallBAPI("BAPI_USER_GETDETAIL",{
+            USERNAME: 'P9TRNG',
+        })
+        res.send(data)
+    } catch (err) {
+        res.status(500).send(err);
+    }
+
+})
+
 // const noderfc = require("node-rfc");
 
-init();
+// init();
 
 /**
  * Name: init
@@ -22,19 +61,21 @@ init();
  * Created By: Suman
  * Created At:
  */
-async function init() {
+// async function init() {
+//     app.get('/', async(req, res) => {
 
-    try {
-        const data = await CallBAPI( "BAPI_VENDOR_GETDETAIL", {
-            VENDORNO: '1',
-            
-        });
-        console.log(data);
-    } catch (e) {
-        console.log(e);
-    }
+//     try {
+//         const data = await CallBAPI( "BAPI_USER_GET_DETAIL",{
+//             USERNAME: 'p9trng',
+//         },);
+//         res.send(data)
+//         // console.log(data);
 
-}
+//     } catch (e) {
+//         console.log(e);
+//     }
+// })
+// }
 
 /**
  * Name: CallBAPI
@@ -44,8 +85,8 @@ async function init() {
  * Created By: Suman
  * Created At:
  */
-async function CallBAPI( FUNCTION_MODULE, props) {
-    try{
+async function CallBAPI(FUNCTION_MODULE, props) {
+    try {
         console.log("Wait processing");
         const Client = await Connect();
         console.log("Getting Data. Please Wait.");
@@ -58,7 +99,7 @@ async function CallBAPI( FUNCTION_MODULE, props) {
                 resolve(res);
             });
         })
-    }catch (e) {
+    } catch (e) {
         return e;
     }
 }
@@ -71,12 +112,12 @@ async function CallBAPI( FUNCTION_MODULE, props) {
  */
 function Connect() {
     const abapSystem = {
-        ashost: '',
-        sysid: '',
-        sysnr: '',
-        user: '',
-        Passwd: '',
-        dest: "",
+        ashost: 'sap.roitech.in',
+        sysid: 'IDS',
+        sysnr: '05',
+        user: 'P9TRNG',
+        Passwd: 'ROITech1234',
+        dest: "MME",
     }
 
     let client = new noderfc.Client(abapSystem);
